0b4d72880c6e6fa5b6cebd8b63631b63a548d045
Selepe Sello <sbkskhalo.kq@gmail.com>
2022-10-03 11:50:43 +0200

    Updated Code Files

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = '......';
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+export const apiEndpoint = '.......'
+// export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'dev-fjp-3wf9.us.auth0.com',
-  clientId: 'RzGQEDmJF4A5OznOSj7zIYBQ1zWTe1dT',
+  domain: '.......',
+  clientId: '.......',
   callbackUrl: 'http://localhost:3000/callback'
 };
